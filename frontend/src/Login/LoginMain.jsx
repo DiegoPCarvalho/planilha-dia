e10d18744e974fe25eb04fc837e710559a894b96
Diego Pontes de Carvalho <diegopontescarvalho@outloo.com>
2022-12-16 17:18:07 -0300

    fase 2.1

--- frontend/src/Login/LoginMain.jsx
+++ frontend/src/Login/LoginMain.jsx
@@ -1,70 +1,97 @@
-import React from  'react';
+import React from 'react';
 import LogoZhaz from '../Assets/Imgs/logoZhaz.png';
 
-export default class LoginMain extends React.Component{
-    login(){
-        return window.location.href ="/Atividade";
+
+
+export default class LoginMain extends React.Component {
+    verificar() {
+        const userErro = document.getElementById("texto_usuario");
+        const login = document.getElementById("user_id").value;
+        const loginErro = document.getElementById("texto_senha");
+        const senha = document.getElementById("senha").value;
+        const textoErro = document.getElementById("texto_erro");
+
+        if ((login === '') || (senha === '')) {
+           textoErro.innerText="Campos não Preenchidos";
+           userErro.innerText = "Usuario:*"
+           userErro.classList.add("text-danger")
+           
+           loginErro.innerText="Senha:*"
+           loginErro.classList.add("text-danger")
+           
+        } else {
+            this.login()
+        }
     }
 
-    newData(){
-        return window.location.href ="/NovoUsuario";
+    mensagem(mensagem) {
+        return mensagem
     }
 
-   render(){
-    return(
-        <div className="d-flex flex-row tamanho">
-                    <div className="h-100 w-100 transparencia rounded">
-                        <div className="row">
-                            <div className="col-12 d-flex justify-content-center mb-5 mt-5">
-                                <img src={LogoZhaz} alt="" />
-                            </div>
+    login() {
+        return window.location.href = "/Atividade";
+    }
+
+    newData() {
+        return window.location.href = "/NovoUsuario";
+    }
+
+    render() {
+        return (
+            <div className="d-flex flex-row tamanho">
+                <div className="h-100 w-100 transparencia rounded">
+                    <div className="row">
+                        <div className="col-12 d-flex justify-content-center mb-5 mt-5">
+                            <img src={LogoZhaz} alt="" />
                         </div>
-                        <div className="row">
-                            <div className="col-12 mb-3 mt-3">
-                                <div className="row mx-5">
-                                    <div className="col-12">
-                                        <label for="User" className="fw-bold h5">Usuário:</label>
-                                    </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 mb-3 mt-3">
+                            <div className="row mx-5">
+                                <div className="col-12">
+                                    <label for="User" id="texto_usuario" className="fw-bold h5">Usuário:</label>
                                 </div>
-                                <div className="row mx-5">
-                                    <div className="col-12">
+                            </div>
+                            <div className="row mx-5">
+                                <div className="col-12">
                                     <div className="input-group mb-3">
                                         <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-user"></i></span>
-                                        <input type="text" class="form-control" placeholder="Usuário" aria-label="Username" aria-describedby="basic-addon1" />
-                                    </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-12">
-                                <div className="row mx-5">
-                                    <div className="col-12">
-                                        <label for="User" className="fw-bold h5">Senha:</label>
-                                    </div>
-                                </div>
-                                <div className="row mx-5 mb-5">
-                                    <div className="col-12">
-                                    <div className="input-group mb-3">
-                                        <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-key"></i></span>
-                                        <input type="password" class="form-control" placeholder="Senha" aria-label="Senha" aria-describedby="basic-addon1" />
-                                    </div>
+                                        <input type="text" id="user_id" className="form-control" placeholder="E-MAIL" aria-label="Username" aria-describedby="basic-addon1" />
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="row mt-5 d-flex justify-content-around">
-                            <div className="col-6 d-flex justify-content-center">
-                                <button className="btn btn-info fw-bold text-light" onClick={(e) => this.login(e)}>
-                                    Entrar
-                                </button>
+                        <div className="col-12">
+                            <div className="row mx-5">
+                                <div className="col-12">
+                                    <label id="texto_senha" for="User" className="fw-bold h5">Senha:</label>
+                                </div>
                             </div>
-                            <div className="col-6">
-                                <button className="btn btn-info text-light fw-bold" onClick={(e) => this.newData(e)}>
-                                    Novo Usuário
-                                </button>
+                            <div className="row mx-5 mb-5">
+                                <div className="col-12">
+                                    <div className="input-group mb-3">
+                                        <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-key"></i></span>
+                                        <input type="password" id="senha" class="form-control" placeholder="Senha" aria-label="Senha" aria-describedby="basic-addon1" />
+                                    </div>
+                                </div>
                             </div>
-                        </div>    
+                            <p id="texto_erro" className='text-danger fw-bold h6 mx-3'></p>
+                        </div>
+                    </div>
+                    <div className="row mt-5 d-flex justify-content-around">
+                        <div className="col-6 d-flex justify-content-center">
+                            <button className="btn btn-success fw-bold text-light" onClick={(e) => this.verificar(e)}>
+                                Entrar
+                            </button>
+                        </div>
+                        <div className="col-6">
+                            <button className="btn btn-info text-light fw-bold" onClick={(e) => this.newData(e)}>
+                                Novo Usuário
+                            </button>
+                        </div>
                     </div>
                 </div>
-    )
-   }
+            </div>
+        )
+    }
 }
--- frontend/src/Login/NovoLogin.jsx
+++ frontend/src/Login/NovoLogin.jsx
@@ -1,26 +1,29 @@
-import React from  'react';
+import React from 'react';
 import LogoZhaz from '../Assets/Imgs/logoZhaz.png';
 
-export default class LoginMain extends React.Component{
+import { Link } from 'react-router-dom';
 
-    save(){
-        return window.location.href ="/";
+export default class LoginMain extends React.Component {
+
+    save() {
+        return window.location.href = "/";
     }
-    cancel(){
-        return window.location.href ="/";
+    cancel() {
+        return window.location.href = "/";
     }
 
-   render(){
-    return(
-        <div className="d-flex flex-row tamanho">
-                    <div className="h-100 w-100 transparencia rounded">
-                        <div className="row">
-                            <div className="col-12 d-flex justify-content-center mb-5 mt-5">
-                                <img src={LogoZhaz} alt="" />
-                            </div>
+    render() {
+        return (
+            <div className="d-flex flex-row tamanho">
+                <div className="h-100 w-100 transparencia rounded">
+                    <div className="row">
+                        <div className="col-12 d-flex justify-content-center mt-5">
+                            <img src={LogoZhaz} alt="" />
                         </div>
-                        <div className="row">
-                            <div className="col-12 mb-3 mt-3">
+                    </div>
+                    <div className="row mt-2">
+                        <div className="row mb-3">
+                        <div className="col-12">
                                 <div className="row mx-5">
                                     <div className="col-12">
                                         <label for="User" className="fw-bold h5">Usuário:</label>
@@ -28,10 +31,25 @@ export default class LoginMain extends React.Component{
                                 </div>
                                 <div className="row mx-5">
                                     <div className="col-12">
-                                    <div className="input-group mb-3">
-                                        <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-user"></i></span>
-                                        <input type="text" class="form-control" placeholder="Usuário" aria-label="Username" aria-describedby="basic-addon1" />
+                                        <div className="input-group mb-3">
+                                            <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-user"></i></span>
+                                            <input type="text" class="form-control" placeholder="Usuário" aria-label="Username" aria-describedby="basic-addon1" />
+                                        </div>
                                     </div>
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <div className="row mx-5">
+                                    <div className="col-12">
+                                        <label for="User" className="fw-bold h5">E-mail:</label>
+                                    </div>
+                                </div>
+                                <div className="row mx-5">
+                                    <div className="col-12">
+                                        <div className="input-group mb-3">
+                                            <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-envelope"></i></span>
+                                            <input type="text" class="form-control" placeholder="E-mail" aria-label="Username" aria-describedby="basic-addon1" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -41,12 +59,12 @@ export default class LoginMain extends React.Component{
                                         <label for="User" className="fw-bold h5">Senha:</label>
                                     </div>
                                 </div>
-                                <div className="row mx-5 mb-3">
+                                <div className="row mx-5 mb-2">
                                     <div className="col-12">
-                                    <div className="input-group mb-3">
-                                        <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-key"></i></span>
-                                        <input type="password" class="form-control" placeholder="Senha" aria-label="Senha" aria-describedby="basic-addon1" />
-                                    </div>
+                                        <div className="input-group mb-2">
+                                            <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-key"></i></span>
+                                            <input type="password" class="form-control" placeholder="Senha" aria-label="Senha" aria-describedby="basic-addon1" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -58,30 +76,30 @@ export default class LoginMain extends React.Component{
                                 </div>
                                 <div className="row mx-5 mb-3">
                                     <div className="col-12">
-                                    <div className="input-group mb-3">
-                                        <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-key"></i></span>
-                                        <input type="password" class="form-control" placeholder="Confirme a Senha" aria-label="ConfirSenha" aria-describedby="basic-addon1" />
-                                    </div>
+                                        <div className="input-group mb-3">
+                                            <span className="input-group-text bg-warning" id="basic-addon1"><i className="fa fa-key"></i></span>
+                                            <input type="password" class="form-control" placeholder="Confirme a Senha" aria-label="ConfirSenha" aria-describedby="basic-addon1" />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="row mt-5">
-                        <div className="col-2 d-flex justify-content-start align-items-center">
-                                <a href="/" className='btn btn-dark mx-3 fw-bold'>Voltar</a>
+                            <div className="col-2 d-flex justify-content-start align-items-center">
+                                <Link to="/" className='btn btn-dark mx-3 fw-bold'>Voltar</Link>
                             </div>
                             <div className="col-9 d-flex justify-content-end">
                                 <button className="btn btn-primary fw-bold mx-2" onClick={(e) => this.save(e)}>
                                     Salvar
                                 </button>
                                 <button className="btn btn-danger fw-bold mx-1">
-                                    Cancelar    
+                                    Cancelar
                                 </button>
-                                {/* <a href="/Atividade" className="btn btn-info bg-gradient text-light  fw-bold">Entrar</a> */}
                             </div>
-                        </div>    
+                        </div>
                     </div>
                 </div>
-    )
-   }
+            </div>
+        )
+    }
 }
