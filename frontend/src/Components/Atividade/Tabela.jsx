d28a4bb9297bb42d5db3d2b2b5df784ba9810e99
Diego Pontes de Carvalho <diegopontescarvalho@outloo.com>
2023-09-06 16:45:50 -0300

    novas Atualizacoes 06/09/2023

--- frontend/src/Components/Atividade/Tabela.jsx
+++ frontend/src/Components/Atividade/Tabela.jsx
@@ -7,6 +7,7 @@ import { confirmAlert } from "react-confirm-alert";
 import "react-confirm-alert/src/react-confirm-alert.css";
 
 import $ from 'jquery';
+import { setAnimation } from 'highcharts';
 
 const initialState = {
     Atividade: {
@@ -27,7 +28,9 @@ const initialState = {
         Status: '',
         Tecnico: localStorage.usuario
     },
-    list: []
+    list: [],
+    mes: [],
+    ano: []
 }
 
 
@@ -43,21 +46,35 @@ export default class Tabela extends React.Component {
 
     componentWillMount() {
         this.retornoTabela()
+        this.retornoData()
+        this.pesquisar()  
+           
+    }
 
+    pesquisar(){
         $(document).ready(function () {
             setTimeout(() => {
                 $('#tabela').DataTable({
                     language: { url: '//cdn.datatables.net/plug-ins/1.11.1/i18n/pt_br.json', },
-                    dom: 'Bfrtip',
-                    buttons: [
-                        'csv', 'excel', 'print'
-                    ]
                 });
-            }, 1000)
+            }, 2000)
         });
     }
 
-    async retornoTabela() {
+    
+
+    retornoData() {
+        const data = new Date();
+        const mes = data.getMonth() + 1;
+        const ano = data.getFullYear();
+
+        return this.setState ({
+            ano: ano,
+            mes: mes
+        })
+    }
+
+     async retornoTabela() {
         const tabelaNome = await axios(baseUrl).then(resp => resp.data)
         let dadoNome = []
 
@@ -65,7 +82,7 @@ export default class Tabela extends React.Component {
 
 
         for (let i = 0; i < tabelaNome.length; i++) {
-            if (localStorage.usuario === tabelaNome[i].Tecnico) {
+            if ((localStorage.usuario === tabelaNome[i].Tecnico)&&(this.state.ano === tabelaNome[i].Ano)&&(this.state.mes === tabelaNome[i].Mes)) {
                 dadoNome.push({
                     id: tabelaNome[i].id,
                     Data: tabelaNome[i].Data,
@@ -89,8 +106,57 @@ export default class Tabela extends React.Component {
         }
 
         return this.setState({ list: dadoNome })
+        
+
+        
     }
 
+    
+    async buscarDados(mes, ano){
+        const tabelaNome = await axios(baseUrl).then(resp => resp.data)
+        let dadoNomeFill = []   
+
+        await this.formataData(tabelaNome)
+
+            for (let i = 0; i < tabelaNome.length; i++) {
+                if ((localStorage.usuario === tabelaNome[i].Tecnico)&&(ano === `${tabelaNome[i].Ano}`)&&(mes === `${tabelaNome[i].Mes}`)) {
+                    dadoNomeFill.push({
+                        id: tabelaNome[i].id,
+                        Data: tabelaNome[i].Data,
+                        Dia: tabelaNome[i].Dia,
+                        Mes: tabelaNome[i].Mes,
+                        Ano: tabelaNome[i].Ano,
+                        OS: tabelaNome[i].OS,
+                        Cliente: tabelaNome[i].Cliente,
+                        Equipamento: tabelaNome[i].Equipamento,
+                        Modelo: tabelaNome[i].Modelo,
+                        NS: tabelaNome[i].NS,
+                        Servico: tabelaNome[i].Servico,
+                        Placa: tabelaNome[i].Placa,
+                        Classificacao: tabelaNome[i].Classificacao,
+                        Contrato: tabelaNome[i].Contrato,
+                        Observacao: tabelaNome[i].Observacao,
+                        Tecnico: tabelaNome[i].Tecnico,
+                        Status: tabelaNome[i].Status
+                    })
+                }
+            }
+
+            return this.setState({ list: dadoNomeFill})
+ 
+            // console.log(dadoNome, ano, mes)
+        
+    }
+
+   
+    chamarDados(){
+        const ano = document.getElementById("ano").value;
+        const mes = document.getElementById("mes").value;
+        this.buscarDados(mes, ano)
+
+       
+    }
+   
 
     dataCerta(dia) {
         const dt = new Date(dia).toLocaleDateString();
@@ -98,7 +164,7 @@ export default class Tabela extends React.Component {
     }
 
 
-    async formataData(dataItem){
+    async formataData(dataItem) {
         for (var i = 0; i < dataItem.length; i++) {
             var dataA = dataItem[i];
             var dataF = await dataA.Data.replace(/(\d*)-(\d*)-(\d*)T(\d*):(\d*).*/, '$3/$2/$1');
@@ -177,7 +243,7 @@ export default class Tabela extends React.Component {
             this.mensagemSalvo()
         }
     }
-    
+
     formulario() {
         return (
             <form className="row g-3" action="javascript:myFunction(); return false;">
@@ -305,7 +371,7 @@ export default class Tabela extends React.Component {
                             <div className="form-group">
                                 <label className='fw-bold'>Contrato: </label>
                                 <select class="form-select" aria-label="Default select example"
-                                name="Contrato" id ="Contrato"
+                                    name="Contrato" id="Contrato"
                                     onChange={e => this.updateField(e)}
                                     value={this.state.Atividade.Contrato}
                                     required>
@@ -389,7 +455,7 @@ export default class Tabela extends React.Component {
                         <th>Modelo</th>
                         <th>NS</th>
                         <th>Serviço</th>
-                       {/* <th>Classificação</th>
+                        {/* <th>Classificação</th>
                         <th>Status</th> */}
                         <th>Ações</th>
                     </tr>
@@ -457,7 +523,48 @@ export default class Tabela extends React.Component {
                 <div className='mb-3'>
                     <i className="fa fa-table fa-4x"></i>
                 </div>
-                <div>
+                <div className="row mt-4 mb-4 d-flex justify-content-center">
+                    <div className="col-2 d-flex flex-row justify-content-end align-items-center">
+                        <i className="fa fa-search fa-2x text-danger" /> 
+                    </div>
+                    <div className="col-2">
+                        <label className='fw-bold'>Mês: </label>
+                        <select id="mes" class="form-select" aria-label="Default select example">
+                            <option selected disabled>Todos</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                            <option>11</option>
+                            <option>12</option>
+                        </select>
+                    </div>
+                    <div className="col-2">
+                        <label className='fw-bold'>Ano: </label>
+                        <select id="ano" class="form-select" aria-label="Default select example">
+                            <option selected disabled>Todos</option>
+                            <option>2022</option>
+                            <option>2023</option>
+                            <option>2024</option>
+                            <option>2025</option>
+                            <option>2026</option>
+                            <option>2027</option>
+                            <option>2028</option>
+                            <option>2029</option>
+                            <option>2030</option>
+                        </select>
+                    </div>
+                    <div className="col-2 d-flex align-items-end">
+                        <button className="btn btn-success fw-bold" onClick={() => this.chamarDados()}>Buscar</button>
+                    </div>
+                </div>
+                <div className="mt-4">
                     {this.renderTable()}
                 </div>
             </div>
