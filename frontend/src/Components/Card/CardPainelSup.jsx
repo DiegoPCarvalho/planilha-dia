e710bb00e78e3f81e7216a8ab496b825e4ec354b
Diego Pontes de Carvalho <diego.pontes@zhaz.com.br>
2024-04-11 17:38:58 -0300

    fase v1.1 de atualização

---
+++ frontend/src/Components/Card/CardPainelSup.jsx
@@ -0,0 +1,38 @@
+import Card from 'react-bootstrap/Card';
+
+function CardSuporte(props) {
+  return (
+    <Card border={'success'} style={{ overflow: 'auto' }} className='w-100'>
+      <Card.Body>
+        <Card.Text>
+          <div className='row'>
+            <div className='col-4 fw-bold'><span className='bg-dark text-white p-1 rounded'>OS:</span> <span style={{ marginLeft: 1 }} className='p-1 bg-success rounded text-white'>{props.OS}</span></div>
+            <div className='col-5 fw-bold'><span className='bg-dark text-white p-1 rounded'>DATA:</span> <span style={{ marginLeft: 1 }} className='p-1 bg-success rounded text-white'>{props.Data}</span></div>
+            {props.contrato ? (
+              <div className='col-3 fw-bold'><span className='bg-dark text-white p-1 rounded'>SLA:</span> <span style={{ marginLeft: 1 }} className='p-1 bg-success rounded text-white'>{props.Sla}</span></div>
+            ) : false}
+
+          </div>
+        </Card.Text>
+        <Card.Text>
+          <div className="row">
+            <div className='fw-bold'><span className='bg-dark text-white p-1 rounded'>EQUIPAMENTO:</span> <span>{props.Equipamento}</span></div>
+          </div>
+        </Card.Text>
+        <Card.Text>
+          <div className='row'>
+            <div className='col-5 fw-bold'><span className='bg-dark text-white p-1 rounded'>NS:</span> <span style={{ marginLeft: 1 }} className='p-1 bg-danger rounded text-white'>{props.NS}</span></div>
+            <div className='col-7 fw-bold'><span className='bg-dark text-white p-1 rounded'>AGENTE:</span> <span style={{ marginLeft: 1 }}>{props.Agente}</span></div>
+          </div>
+        </Card.Text>
+        <Card.Text>
+        <div className='row'>
+            <div className='fw-bold'><span className='bg-dark text-white p-1 rounded'>OBS:</span> <span>{props.Obs}</span></div>
+          </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default CardSuporte;
--- frontend/src/Components/PainelSuporte/Avulso/Avulso.jsx
+++ frontend/src/Components/PainelSuporte/Avulso/Avulso.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import axios from 'axios';
 import SupGif from '../../../Assets/gifs/tabSup.gif';
+import Url from '../../Url/Url';
+import CardSuporte from "../../Card/CardPainelSup";
+
 
 const initialState = {
-
+    coletores: [],
+    leitores: [],
+    impressoras: [],
+    banco: [],
+    registros: []
 }
 
 const estilo = {
@@ -11,9 +19,79 @@ const estilo = {
     borderRadius: '50%'
 }
 
+const baseUrlBanco = Url("Banco");
+
 export default class Avulso extends React.Component {
 
-    renderTable(Equip, dados) {
+    state = { ...initialState }
+
+    componentDidMount() {
+        axios(`${baseUrlBanco}`).then(resp => {
+            this.setState({ banco: resp.data })
+        })
+
+        setTimeout(() => {
+            axios(`http://${this.state.banco[0].nome}/suporteAvulso`).then(resp => {
+                this.setState({ registros: resp.data })
+            })
+
+        }, 1000);
+
+        setTimeout(() => {
+            this.buscarPorEquipamento()
+        }, 3000);
+
+        
+
+    }
+
+    buscarPorEquipamento = () => {
+        const banco = this.state.registros
+        let impressoras = []
+        let coletores = []
+        let leitores = []
+
+        banco.map(bnc => {
+            var c = `${bnc.Equipamento}`
+            if (c.match(/IMPRESSORA/)) {
+                impressoras.push({
+                    ...bnc
+                })
+            }
+            if (c.match(/COLETOR/)) {
+                coletores.push({
+                    ...bnc
+                })
+            }
+            if (c.match(/TERMINAL/)) {
+                coletores.push({
+                    ...bnc
+                })
+            }
+            if (c.match(/LEITOR/)) {
+                leitores.push({
+                    ...bnc
+                })
+            }
+        })
+
+
+        return this.setState({
+            impressoras,
+            coletores,
+            leitores
+        }) 
+
+    }
+
+    formatarData = (data) =>{
+        const dt = new Date(data).toLocaleDateString()
+        return dt
+    }
+
+
+
+    renderTable(Equip, dados, retorno) {
         return (
             <table style={{ maxHeight: '50vh' }} className="table  mt-3">
                 <thead className='table-dark'>
@@ -22,20 +100,36 @@ export default class Avulso extends React.Component {
                     </tr>
                 </thead>
                 <tbody style={{ overflow: 'auto', height: 400 }} className="d-block">
-                    {dados.length === 0 ? 
+                    {dados.length === 0 ?
                         this.renderBuscando()
-                     : ({ dados })}
+                        : (retorno)}
                 </tbody>
             </table>
         )
     }
 
+    renderRows(equipamentos) {
+        return equipamentos.map((equipamento, i) => {
+            return (
+                <tr className="d-flex">
+                    <td key={i} className="flex-fill d-flex justify-content-center">
+                        <CardSuporte OS={equipamento.OS} Data={this.formatarData(equipamento.Data)} 
+                                Equipamento={equipamento.Equipamento} NS={equipamento.NS}
+                                Agente={equipamento.AgenteComercial} Obs={equipamento.ObservacaoEquip}
+                            />
+                        
+                    </td>
+                </tr>
+            )
+        })
+    }
+
     renderBuscando() {
         return (
-                <div className="d-flex justify-content-center align-items-center">
-                    <img src={SupGif} alt="" style={estilo} />
-                    <span className="fw-bold mx-2">Aguando suportes!!</span>
-                </div>
+            <div className="d-flex justify-content-center align-items-center">
+                <img src={SupGif} alt="" style={estilo} />
+                <span className="fw-bold mx-2">Aguardando suportes!!</span>
+            </div>
         )
     }
 
@@ -49,14 +143,17 @@ export default class Avulso extends React.Component {
                 </div>
                 <div className="row">
                     <div className='col-12 col-md-4'>
-                        {this.renderTable('Coletor', [])}
+                        {this.renderTable('Coletor/Terminal', this.state.coletores, this.renderRows(this.state.coletores))}
                     </div>
                     <div className='col-12 col-md-4 '>
-                        {this.renderTable('Leitor', [])}
+                        {this.renderTable('Leitor', this.state.leitores, this.renderRows(this.state.leitores))}
                     </div>
                     <div className='col-12 col-md-4 '>
-                        {this.renderTable('Impressora', [])}
+                        {this.renderTable('Impressora', this.state.impressoras, this.renderRows(this.state.impressoras))}
                     </div>
+                </div>
+                <div>
+                    {/* <CardSuporte OS={1234} Data={'27/04/2024'} contrato /> */}
                 </div>
             </>
         )
